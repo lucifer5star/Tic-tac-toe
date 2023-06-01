5bc4f7d27aaa946586eac049ab7dda082af7b8d0
Dhiraj kargeti <dhirajkargeti.cse1@bvp.edu.in>
2023-06-02 01:06:50 +0530

    display draw message. apply styles for status message component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import './styles.scss';
 import Board from './components/Board';
 import React, { useState } from 'react';
 import { calculateWinner } from './components/Winner';
+import StatusMessage from './components/StatusMessage';
 
 function App() {
   const [squares,setSquares]= useState(Array(9).fill(null));
     const [isXNext,setIsXNext] = useState(false);
     
     const winner= calculateWinner(squares);
-    const nextPlayer= isXNext? "X": "0"
-    const statusMessage= winner ? `Winner is ${winner}`:`Next player is ${nextPlayer}` ;
-    console.log(winner);
+    
 
     const handleSquareClick=(clickedPosition)=>{
       // console.log(squares);
@@ -30,7 +29,8 @@ function App() {
   
   return (
     <div className='app'>
-      <h2>{statusMessage}</h2>
+      {/* <h2>{statusMessage}</h2> */}
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares}/>
       <Board squares={squares} handleSquareClick={handleSquareClick}/>
     </div>
   );

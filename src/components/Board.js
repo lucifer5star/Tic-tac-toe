6e410764ba41089c8af1f11d4d8d61d35082c77c
Dhiraj kargeti <dhirajkargeti.cse1@bvp.edu.in>
2023-06-01 12:59:21 +0530

    display X when clicking on a square

---
+++ src/components/Board.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Square from './Square'
+const Board = () => {
+    const [squares,setSquares]= useState(Array(9).fill(null));
+    const handleSquareClick=(clickedPosition)=>{
+        console.log(squares);
+        setSquares((currentSquares)=>{
+            return currentSquares.map((squareValue,position)=>{
+                if(clickedPosition===position){
+                    return "X";
+                }
+                return squareValue;
+            })
+        })
+    }
+    const renderSquare=(position,key)=>{
+       return <Square value={squares[position]} onClick={()=>handleSquareClick(position)} />
+    }
+  return (
+    <div className='board'>
+        <div className="board-row">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+            
+        </div>
+        <div className="board-row">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+        </div>
+        <div className="board-row">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+        </div>
+      
+    </div>
+  )
+}
+
+export default Board
---
+++ src/components/Square.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Square = ({value, onClick}) => {
+  return (
+    <button type='button' className='square' onClick={onClick}>{value}</button>
+  )
+}
+
+export default Square
